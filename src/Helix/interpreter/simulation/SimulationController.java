f2393368fa6806ad5cf37025fade3df2547ce0a8
Gonzalo Solera <gonzalo1796@gmail.com>
2017-05-23 18:55:35 +0200

    Added simulationcontroller template

---
+++ src/Helix/interpreter/simulation/SimulationController.java
@@ -0,0 +1,15 @@
+package Helix.interpreter.simulation;
+
+import Helix.interpreter.DroneController;
+
+public class SimulationController extends DroneController implements PathPlannerListener {
+
+    public SimulationController() {
+        
+    }
+
+    @Override
+    public void moveTo() {
+        
+    }
+}
