cd8f30ea8dc0164839030ff9a679ee9bbd438e32
Gonzalo Solera <gonzalo1796@gmail.com>
2017-05-29 12:25:10 +0200

    Implementing error detection during autonomous fly

--- src/Helix/interpreter/controller/librepilot/PathPlanManager.java
+++ src/Helix/interpreter/controller/librepilot/PathPlanManager.java
@@ -12,8 +12,8 @@ public class PathPlanManager implements UAVTalkObjectListener {
     private static final String ACTIVE_WAYPOINT = "WaypointActive";
 
     private PathPlanListener listener;
-    private Waypoint activeWaypoint;
     private FcDevice device;
+    private volatile Waypoint activeWaypoint;
 
     public PathPlanManager(PathPlanListener listener, FcDevice device) {
         this.listener = listener;
@@ -22,40 +22,15 @@ public class PathPlanManager implements UAVTalkObjectListener {
 
         device.requestObject(PATH_STATUS);
         device.setListener(PATH_STATUS, this);
-        device.requestObject(ACTIVE_WAYPOINT);
         device.setListener(ACTIVE_WAYPOINT, this);
     }
 
     public void sendMoveTo(GPSPosition position, GPSPosition homeLocation, double velocity) {
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         activeWaypoint = new Waypoint(position, homeLocation, velocity);
         activeWaypoint.upload(device);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while(activeWaypoint != null) {
+            device.requestObject(ACTIVE_WAYPOINT);
         }
-
-        UAVTalkObjectListener listener = new UAVTalkObjectListener() {
-            @Override
-            public void onObjectUpdate(UAVTalkObject o) {
-                try {
-                    System.out.println("Pos waypoint 1: " + o.getData(0, "Position", 0) + ", " + o.getData(0, "Position", 1) + ", " + o.getData(0, "Position", 2));
-                    System.out.println("Pos waypoint 2: " + o.getData(1, "Position", 0) + ", " + o.getData(1, "Position", 1) + ", " + o.getData(1, "Position", 2));
-                } catch (UAVTalkMissingObjectException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        device.setListener("Waypoint", listener);
-        device.requestObject(ACTIVE_WAYPOINT);
     }
 
     public void sendLand() {
@@ -68,12 +43,12 @@ public class PathPlanManager implements UAVTalkObjectListener {
             try {
                 if(o.getId().equals(device.getObjectTree().getXmlObjects().get(PATH_STATUS).getId())) {
                     float progress = (float) o.getData("fractional_progress");
-                    /*float error = (float) o.getData("error");
-                    if(error != 0) {
-                        listener.onError();
-                    }*/
                     activeWaypoint.progress = progress;
                     listener.onProgressUpdate(progress);
+                    int error = (int) o.getData("Status");
+                    if(error != 3) {
+                        listener.onError();
+                    }
                 } else {
                     if(o == null) {
                         System.out.println("WTF");
@@ -84,9 +59,6 @@ public class PathPlanManager implements UAVTalkObjectListener {
                         listener.onFinishPath();
                         System.out.println("Waypoint: " + active_index);
                         System.out.println("----------------------------------------------");
-                        System.exit(0);
-                    } else {
-                        device.requestObject(ACTIVE_WAYPOINT);
                     }
                 }
             } catch (UAVTalkMissingObjectException e) {
