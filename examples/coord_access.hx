72fa36bfa14123bb3fb6b1a3fd71f0ae61b68f83
daniconde <daniabc2@gmail.com>
2017-05-31 20:19:11 +0200

    More coord_access.hx

--- examples/coord_access.hx
+++ examples/coord_access.hx
@@ -1,10 +1,11 @@
 def main() {
-    take_off()
-    up(1)
-    
+    take_off(10)
     [ini_lat, ini_lng, ini_alt] = get_gps()
-    move([ini_lat, ini_lng + 10, 10])
-
+    move([ini_lat, ini_lng + 10, 20])
+    [sec_lat, sec_lng, sec_alt] = get_gps()
+    next = [sec_lat, sec_lng, 40]
+    move([ini_lat, ini_lng, 30])
+    move(next)
     land()
 }
 
