77ff6df7ea8bed64b7a374e9caa936a2423a27c3
daniconde <daniabc2@gmail.com>
2017-06-01 01:42:19 +0200

    Further improve IntData encapsulation

--- src/Helix/interpreter/IntData.java
+++ src/Helix/interpreter/IntData.java
@@ -4,6 +4,8 @@ import Helix.parser.*;
 
 
 public class IntData extends Data {
+    private static int PREC_I = 10000;
+    private static double PREC_D = PREC_I;
 
     public int value;
 
@@ -19,7 +21,7 @@ public class IntData extends Data {
 
 
     public IntData(double value) {
-        this((new Double(value * 100)).intValue());
+        this((new Double(value * PREC_D)).intValue());
     }
 
 
@@ -31,7 +33,7 @@ public class IntData extends Data {
 
     public double toDouble() {
         double result = (double) value;
-        result = result / 100D;
+        result = result / PREC_D;
         return result;
     }
 
@@ -42,10 +44,10 @@ public class IntData extends Data {
         switch (op) {
             case HelixLexer.PLUS: value += d.value; break;
             case HelixLexer.MINUS: value -= d.value; break;
-            case HelixLexer.MUL: value = (value * d.value) / 100; break;
+            case HelixLexer.MUL: value = (value * d.value) / PREC_I; break;
             case HelixLexer.DIV: 
                 checkDivZero(d);
-                value = new Double((this.toDouble() / d.toDouble()) * 100D).intValue(); 
+                value = new Double((this.toDouble() / d.toDouble()) * PREC_D).intValue(); 
                 break;
             case HelixLexer.MOD: 
                 checkInteger(this);
@@ -82,12 +84,12 @@ public class IntData extends Data {
 
     @Override
     public String toString() {
-        return Double.toString(((double) value) / 100D);
+        return Double.toString(((double) value) / PREC_D);
     }
 
 
     private static void checkInteger(IntData d) {
-        if (d.value % 100 > 0) {
+        if (d.value % PREC_I > 0) {
             throw new RuntimeException("Integer operation on decimal data");
         }
     }
